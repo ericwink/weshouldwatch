b226cd1029a8c2d86902eb746323d6e65e83a1f1
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2023-02-24 15:52:24 -0500

    add type to error

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -9,7 +9,7 @@ export default async function search(req: NextApiRequest, res: NextApiResponse) 
     const { data } = await axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${tmdbKey}&language=en-US&page=1&include_adult=false&query=${searchTerm}`);
     //send an array of objects back to the client
     res.send(data.results);
-  } catch (error) {
+  } catch (error: any) {
     const status = error.response.status;
     const message = error.response.data.status_message;
     res.status(status).send(message);
