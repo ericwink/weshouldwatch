e25e203e49733521511953ac9465da93ecb58dad
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2023-03-02 14:39:40 -0500

    add addUser api and updated create Group api

---
+++ pages/api/group/addUser.ts
@@ -0,0 +1,42 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+const addUser = async (req: NextApiRequest, res: NextApiResponse) => {
+  const { email, groupID } = req.body;
+
+  try {
+    const currentUser = await prisma.user.update({
+      where: {
+        email: email,
+      },
+      data: {
+        groupIDs: {
+          push: groupID,
+        },
+      },
+    });
+
+    const group = await prisma.group.update({
+      where: {
+        id: groupID,
+      },
+      data: {
+        userIDs: {
+          push: currentUser.id,
+        },
+      },
+    });
+
+    console.log({ currentUser });
+    console.log({ group });
+
+    res.send({ message: "user added!" });
+  } catch (error: any) {
+    console.log(error);
+    res.send({ message: error.message });
+  }
+};
+
+export default addUser;
