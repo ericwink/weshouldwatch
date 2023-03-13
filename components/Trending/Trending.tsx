2bcd1915b967c1bc761247d96c72bfcb4639139d
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2023-03-13 14:19:37 -0400

    update trending to display person data

--- components/Trending/Trending.tsx
+++ components/Trending/Trending.tsx
@@ -2,14 +2,15 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import PosterButton from "../PosterButton/PosterButton";
 import PreviewCard from "../PreviewCard/PreviewCard";
-import { Movie } from "@/utilities/interface";
+import ActorButton from "../ActorButton/ActorButton";
+import { Movie, TV, Person } from "@/utilities/interface";
 
 interface Props {
   mediaType: "movie" | "person" | "tv";
 }
 
 const Trending = ({ mediaType }: Props) => {
-  const [data, setData] = useState<Movie[] | null>(null);
+  const [data, setData] = useState<Movie[] | TV[] | Person[] | null>(null);
   const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
@@ -24,6 +25,16 @@ const Trending = ({ mediaType }: Props) => {
 
   if (isLoading) return <h1>loading....</h1>;
   if (!data) return <h1>No data found</h1>;
+
+  if (mediaType === "person") {
+    return (
+      <div>
+        {data.map(each => {
+          return <ActorButton {...each} />;
+        })}
+      </div>
+    );
+  }
 
   return (
     <div>
