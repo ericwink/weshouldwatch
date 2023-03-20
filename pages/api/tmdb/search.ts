f5ee85f74d3437e2def715a4388cbcde3e49c229
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2023-03-20 12:29:52 -0400

    add rough draft of results page

--- pages/api/tmdb/search.ts
+++ pages/api/tmdb/search.ts
@@ -3,7 +3,8 @@ import axios from "axios";
 const tmdbKey = process.env.MOVIE_DB_API;
 
 export default async function search(req: NextApiRequest, res: NextApiResponse) {
-  const { searchTerm } = req.body;
+  const { searchTerm } = req.query;
+  console.log({ searchTerm });
   try {
     const { data } = await axios.get(`https://api.themoviedb.org/3/search/multi?api_key=${tmdbKey}&language=en-US&page=1&include_adult=false&query=${searchTerm}`);
     //send an array of objects back to the client
---
+++ pages/search/[searchTerm].tsx
@@ -0,0 +1,71 @@
+import { useRouter } from "next/router";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Movie, TV, Person } from "@/utilities/interface";
+import PosterButton from "@/components/PosterButton/PosterButton";
+import PreviewCard from "@/components/PreviewCard/PreviewCard";
+import ActorLink from "@/components/ActorLink/ActorLink";
+
+interface IData {
+  movie: Movie[];
+  tv: TV[];
+  person: Person[];
+}
+
+const Results = () => {
+  const router = useRouter();
+  const { searchTerm } = router.query;
+  const [data, setData] = useState<IData | null>(null);
+  const [loading, setLoading] = useState(null);
+
+  useEffect(() => {
+    setLoading(true);
+    const results = fetchResults(searchTerm);
+  }, [searchTerm]);
+
+  const fetchResults = async (searchTerm: string) => {
+    const { data } = await axios.get("/api/tmdb/search", { params: { searchTerm: searchTerm } });
+    const separatedData = separateData(data);
+    setData(separatedData);
+    setLoading(false);
+  };
+
+  const separateData = (data: [Movie | TV | Person]) => {
+    const tempData = { movie: [], tv: [], person: [] };
+    //for each object in data, add to array based on media_type
+    data.forEach(entry => {
+      tempData[entry.media_type].push(entry);
+    });
+    return tempData;
+  };
+
+  if (loading) return <p>Loading...</p>;
+
+  if (data) {
+    const mapResults = (mediaType: string) => {
+      return data[mediaType].map(each => {
+        if (mediaType === "person") {
+          return <ActorLink {...each} />;
+        }
+        return (
+          <PosterButton {...each}>
+            <PreviewCard {...each} />
+          </PosterButton>
+        );
+      });
+    };
+
+    return (
+      <>
+        <p>Movies:</p>
+        {mapResults("movie")}
+        <p>TV:</p>
+        {mapResults("tv")}
+        <p>People:</p>
+        {mapResults("person")}
+      </>
+    );
+  }
+};
+
+export default Results;
