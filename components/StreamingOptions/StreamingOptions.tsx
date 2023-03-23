94ad6e9fbe0d6188c700cfb087c8fcf910980163
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2023-03-23 11:36:08 -0400

    refactor streaming data

--- components/StreamingOptions/StreamingOptions.tsx
+++ components/StreamingOptions/StreamingOptions.tsx
@@ -1,11 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import getPoster from "@/utilities/getPoster";
-import { Props, finalStreamData, AvailabilityData, StreamOptions } from "./interfaces";
+import { Props, AvailabilityData, StreamOptions } from "./interfaces";
 import styles from "./streamingOptions.module.css";
 
 const StreamingOptions = ({ media_type, id }: Props) => {
-  const [streamData, setStreamData] = useState<finalStreamData[] | null>(null);
+  const [streamData, setStreamData] = useState<StreamOptions | null>(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -34,39 +34,47 @@ const StreamingOptions = ({ media_type, id }: Props) => {
         data[host].watchOption.push(watchOption);
       }
     }
-    const array = [];
-    for (let each in data) {
-      array.push({ provider: each, ...data[each] });
-    }
-    return array;
+    return data;
   };
 
   if (loading) return <h1>Loading...</h1>;
   if (!streamData) return <h1>No Data Found</h1>;
 
-  const streamInfo = streamData.map(entry => {
+  const streamInfo = (provider: string) => {
+    const providerData = streamData[provider];
     return (
       <div
-        key={entry.provider}
+        key={provider}
         className={styles.container}
       >
         <img
-          src={getPoster(entry.logo_path, "200")}
-          alt={entry.provider}
+          src={getPoster(providerData.logo_path, "200")}
+          alt={provider}
           className={styles.image}
         />
-        <p>{entry.provider}</p>
+        <p>{provider}</p>
         <div className={styles.options}>
-          {entry.watchOption.map(each => {
-            if (each === "flatrate") return <p>Streaming</p>;
-            return <p>{each}</p>;
+          {providerData.watchOption.map(option => {
+            return buyPurchaseRent(option);
           })}
         </div>
       </div>
     );
-  });
+  };
 
-  return <>{streamInfo}</>;
+  const buyPurchaseRent = (option: "buy" | "rent" | "flatrate") => {
+    const map = { buy: "Buy", rent: "Rent", flatrate: "Streaming" };
+    if (map[option]) return <p>{map[option]}</p>;
+    return <p>{option}</p>;
+  };
+
+  return (
+    <>
+      {Object.keys(streamData).map(provider => {
+        return streamInfo(provider);
+      })}
+    </>
+  );
 };
 
 export default StreamingOptions;
