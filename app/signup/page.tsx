8d7f816491da9310ba9d96171247dcf3885a1b6a
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2023-06-05 17:10:20 -0400

    deploy cleanup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,59 +1,7 @@
-"use client";
+import SignUp from "../components/SignUp";
 
-import { supabase } from "@/lib/supabase";
-import { TextField, Button, Box, Container } from "@mui/material";
-import { useState } from "react";
-
-const gmailSignUp = async () => {
-  let { data, error } = await supabase.auth.signInWithOAuth({
-    provider: "google",
-  });
-  if (error) throw new Error(error.message);
-};
-
-const passwordSignUp = async (email: string, password: string) => {
-  let { data, error } = await supabase.auth.signUp({
-    email: email,
-    password: password,
-  });
-
-  if (error) throw new Error(error.message);
-};
-
-const signUpPage = async () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  return (
-    <Container>
-      <Box
-        display="flex"
-        flexDirection="column"
-        gap={1}
-      >
-        <h1>Signup Page</h1>
-        <TextField
-          label="email"
-          variant="outlined"
-          onChange={e => setEmail(e.target.value)}
-        />
-        <TextField
-          label="password"
-          variant="outlined"
-          onChange={e => setPassword(e.target.value)}
-        />
-        <Button
-          variant="contained"
-          onClick={() => {
-            passwordSignUp(email, password);
-          }}
-        >
-          Sign Up With Email
-        </Button>
-        <button onClick={gmailSignUp}>Sign Up With Google</button>
-      </Box>
-    </Container>
-  );
+const signUpPage = () => {
+  return <SignUp />;
 };
 
 export default signUpPage;
