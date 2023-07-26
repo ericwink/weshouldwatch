4d5f41052fc8fa8721f9ebb518d84a5fee7253a7
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2023-07-26 17:07:16 -0400

    add delete button to card and functionality. put it on card due to caching issues

--- src/components/ConfirmDelete.tsx
+++ src/components/ConfirmDelete.tsx
@@ -5,16 +5,22 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import { TextField } from "@mui/material";
 
 interface Props {
   confirmDelete: () => void;
   warningMessage: string;
   showDeleteModal: boolean;
   setShowDeleteModal: React.Dispatch<React.SetStateAction<boolean>>;
+  extraSecure?: boolean;
+  extraSecureCheck?: string;
 }
 
-export default function ConfirmDelete({ showDeleteModal, setShowDeleteModal, warningMessage, confirmDelete }: Props) {
+export default function ConfirmDelete({ showDeleteModal, setShowDeleteModal, warningMessage, confirmDelete, extraSecure = false, extraSecureCheck }: Props) {
+  const [input, setInput] = useState("");
+
   const handleClose = () => {
+    setInput("");
     setShowDeleteModal(false);
   };
 
@@ -31,11 +37,25 @@ export default function ConfirmDelete({ showDeleteModal, setShowDeleteModal, war
       aria-describedby="alert-dialog-description"
     >
       <DialogTitle id="alert-dialog-title">{"Are you sure you want to delete?"}</DialogTitle>
-      <DialogContent>
+      <DialogContent sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
         <DialogContentText id="alert-dialog-description">{`${warningMessage} Are you sure you want to delete?`}</DialogContentText>
+        {extraSecure && (
+          <>
+            <DialogContentText>{`Enter the name of the group "${extraSecureCheck}" to proceed with deletion`}</DialogContentText>
+            <TextField
+              onChange={e => setInput(e.target.value)}
+              value={input}
+            />
+          </>
+        )}
       </DialogContent>
       <DialogActions>
-        <Button onClick={handleConfirm}>Delete</Button>
+        <Button
+          onClick={handleConfirm}
+          disabled={extraSecure && input !== extraSecureCheck}
+        >
+          Delete
+        </Button>
         <Button
           onClick={handleClose}
           autoFocus
---
+++ src/components/GroupControl/DeleteGroup.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { Button, IconButton } from "@mui/material";
+import ConfirmDelete from "../ConfirmDelete";
+import { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { toast } from "react-toastify";
+import { deleteGroup } from "@/src/lib/serverActions";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
+
+interface Props {
+  group_name: string | null;
+  id: number;
+}
+
+const DeleteGroup = ({ id, group_name }: Props) => {
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+
+  const { mutate: deleteThisGroup } = useMutation({
+    mutationFn: async () => {
+      const result = await deleteGroup(id);
+      if (result.error) throw new Error(result.message);
+    },
+    onSuccess: () => {
+      toast.success("Group deleted successfully", { theme: "colored" });
+    },
+    onError: (error: any) => {
+      toast.error(error.message, { theme: "colored" });
+    },
+  });
+
+  return (
+    <div>
+      <IconButton
+        color="error"
+        onClick={() => setShowDeleteModal(true)}
+      >
+        <DeleteForeverIcon />
+      </IconButton>
+      <ConfirmDelete
+        confirmDelete={() => deleteThisGroup()}
+        showDeleteModal={showDeleteModal}
+        setShowDeleteModal={setShowDeleteModal}
+        warningMessage="This action will delete ALL group data and cannot be undone."
+        extraSecure={true}
+        extraSecureCheck={group_name as string}
+      />
+    </div>
+  );
+};
+
+export default DeleteGroup;
