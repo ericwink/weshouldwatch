7a27664d9cd334dbe23d90880cb92d37aa02d7a4
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2024-03-30 16:37:24 -0400

    remove tester optional

--- src/components/Account/SubscriptionDetails.tsx
+++ src/components/Account/SubscriptionDetails.tsx
@@ -42,7 +42,7 @@ const SubscriptionDetails = async ({ userAccount }: { userAccount: UserAccount }
     <div>
       <Typography textAlign="center">Current Subscription: {canceled ? "Canceled" : "Active"}</Typography>
       <Typography textAlign="center">
-        {canceled ? "Premium Ends On:" : "Next Billing Date:"} {formattedDate || "Tester"}
+        {canceled ? "Premium Ends On:" : "Next Billing Date:"} {formattedDate}
       </Typography>
     </div>
   );
