d7a86b0dcfa3e986e88da95405fdfddd0b3f20f1
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2024-09-15 17:45:02 -0400

    update nav on group id layout

--- src/app/mygroups/[id]/layout.tsx
+++ src/app/mygroups/[id]/layout.tsx
@@ -1,4 +1,13 @@
+"use client";
+
 import Link from "next/link";
+import Box from "@mui/material/Box";
+import BottomNavigation from "@mui/material/BottomNavigation";
+import BottomNavigationAction from "@mui/material/BottomNavigationAction";
+import RestoreIcon from "@mui/icons-material/Restore";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import { usePathname } from "next/navigation";
 
 interface Props {
   children: React.ReactNode;
@@ -8,13 +17,39 @@ interface Props {
 }
 
 const GroupLayout = ({ children, params }: Props) => {
+  const pathname = usePathname();
+
+  const getNavValue = () => {
+    if (pathname.includes("movies")) return 0;
+    if (pathname.includes("tv")) return 1;
+    return 2;
+  };
+
   return (
-    <section>
-      <nav>
-        <Link href={`/mygroups/${params.id}/movies`}>Movies</Link>
-        <Link href={`/mygroups/${params.id}/tv`}>TV</Link>
-        <Link href={`/mygroups/${params.id}/info`}>Group Info</Link>
-      </nav>
+    <section className="w-full flex flex-col items-center">
+      <Box sx={{ width: 500 }}>
+        <BottomNavigation value={getNavValue()} showLabels>
+          <BottomNavigationAction
+            label="Movies"
+            LinkComponent={Link}
+            href={`/mygroups/${params.id}/movies`}
+            icon={<RestoreIcon />}
+          />
+          <BottomNavigationAction
+            label="TV Shows"
+            href={`/mygroups/${params.id}/tv`}
+            LinkComponent={Link}
+            icon={<FavoriteIcon />}
+          />
+          <BottomNavigationAction
+            label="Group Info"
+            LinkComponent={Link}
+            icon={<LocationOnIcon />}
+            href={`/mygroups/${params.id}/info`}
+          />
+        </BottomNavigation>
+      </Box>
+
       {children}
     </section>
   );
