2ae7c22d7bd7490315baf5aefbf0656052433999
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2024-09-15 17:56:13 -0400

    commit before generating supabase types

--- src/app/mygroups/[id]/movies/page.tsx
+++ src/app/mygroups/[id]/movies/page.tsx
@@ -1,4 +1,15 @@
-const GroupMoviesPage = () => {
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Database } from "@/src/lib/database.types";
+
+const GroupMoviesPage = async () => {
+  const supabase = createServerComponentClient<Database>({ cookies });
+  const { data: session } = await supabase.auth.getSession();
+  if (!session.session) redirect("/login");
+
+  //   const movies = await supabase.from('group_media').select('id,')
+
   return <div>Movies Page</div>;
 };
 
