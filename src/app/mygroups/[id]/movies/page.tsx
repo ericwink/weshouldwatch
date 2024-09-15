e999cf361ad9091823474bf79261f64eadbad5ea
Eric Winkelspecht <eric.winkelspecht@gmail.com>
2024-09-15 18:17:01 -0400

    add fetch for movies page

--- src/app/mygroups/[id]/movies/page.tsx
+++ src/app/mygroups/[id]/movies/page.tsx
@@ -3,12 +3,23 @@ import { redirect } from "next/navigation";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { Database } from "@/src/lib/database.types";
 
-const GroupMoviesPage = async () => {
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+const GroupMoviesPage = async ({ params }: Props) => {
   const supabase = createServerComponentClient<Database>({ cookies });
   const { data: session } = await supabase.auth.getSession();
   if (!session.session) redirect("/login");
 
-  //   const movies = await supabase.from('group_media').select('id,')
+  const movies = await supabase
+    .from("group_media")
+    .select(`media_id,watched, media(*)`)
+    .eq("group_id", `${params.id}`);
+
+  console.log(movies);
 
   return <div>Movies Page</div>;
 };
